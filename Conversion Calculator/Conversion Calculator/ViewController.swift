f1459faf7e72fb85fe9c0e28e2b4ef807776675f
Cody Polton <cj.polton@gmail.com>
2019-10-27 21:07:34 -0500

    Starting project

---
+++ Conversion Calculator/Conversion Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversion Calculator
+//
+//  Created by Cody Polton on 27/10/2019.
+//  Copyright © 2019 Cody Polton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
